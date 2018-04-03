b31d3d662ba26511471902441d28b87b093a3a48
charleschang213 <869048286@qq.com>
2018-04-03 09:55:17 +0800

    UFSet Implemented

---
+++ UFSet/UFSet.h
@@ -0,0 +1,47 @@
+#ifndef UFSET_H
+#define UFSET_H
+#include <iostream>
+
+namespace Charles{
+    template<class T>
+    class UFSet{
+        public:
+            UFSet(int n):Size(n){
+                s = new T[n];
+                parent = new int[n];
+                for (int i=0;i<n;i++) {
+                    parent[i] = -1;
+                    s[i] = i;
+                }
+            }
+            UFSet(int n,T* d):Size(n){
+                s = new T[n];
+                parent = new int[n];
+                for (int i=0;i<n;i++) {
+                    parent[i] = -1;
+                    s[i] = d[i];
+                }
+            }
+            ~UFSet(){delete s;delete parent;}
+            void Union(int x,int y){
+                parent[x] = y;
+            }
+            int Find(int i){
+                int r=i;
+                int t,l;
+                while (parent[r]>0){r = parent[r];}
+                if (r!=i){
+                    for (t = i;parent[t]!=r;t=l){
+                        l = parent[t];parent[t] = r;
+                    }
+                }
+                return r;
+            }
+        private:
+            int Size;
+            T* s;
+            int* parent;
+    };
+}
+
+#endif
